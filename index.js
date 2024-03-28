947ac7421b542e45dbaef3383306f1c92166c933
Gonzalo <gonzalobuasso@gmail.com>
2024-03-28 15:32:02 -0300

    con console log

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-//   console.log(`New Change Listening on port ${port}`)
+   console.log(`con console log Change Listening on port ${port}`)
 })
